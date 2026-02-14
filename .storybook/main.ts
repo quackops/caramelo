825e4c624da0417070ae6c3a5afebba25a240b4a
pmqueiroz <pedroqueiroz593@gmail.com>
2026-02-14 16:29:31 -0300

    chore: increate chunkSizeWarningLimit to 2mb

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,13 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [],
   framework: '@storybook/react-vite',
+  viteFinal: (config) => {
+    config.build = {
+      ...config.build,
+      chunkSizeWarningLimit: 2000,
+    };
+    return config;
+  },
 };
 
 export default config;
