0917fb209f46b65bef9e6400097e3b9f6c18063a
pmqueiroz <pedroqueiroz593@gmail.com>
2026-02-15 15:17:33 -0300

    chore: fix lint errors

--- src/components/button/button.spec.tsx
+++ src/components/button/button.spec.tsx
@@ -4,7 +4,7 @@ import { Button } from './button';
 
 describe('Button', () => {
   it('renders a button element', () => {
-    render(<Button variant="primary">Click me</Button>);
+    render(<Button variant="brand">Click me</Button>);
     expect(
       screen.getByRole('button', { name: /click me/i }),
     ).toBeInTheDocument();
